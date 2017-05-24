5281a585cdf7f81b2c7fb6e75d9efe32398dfbdd
Anita Torrence <anita.torrence@gmail.com>
2017-05-24 12:07:37 -0400

    New kata exercise added

---
+++ More_Code_Exercises/VowelCount.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace More_Code_Exercises
+{
+    public class VowelCount
+    {
+        // Return the number(count) of vowels in the given string.
+        //We will consider a, e, i, o, and u as vowels for this Kata.
+        public static int GetVowelCount(string str)
+        {
+            int vowelCount = 0;
+            str = str.ToLower();
+            // Your code here
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str.Substring(i,1) == "a")
+                {
+                    vowelCount++;
+                }
+                if (str.Substring(i, 1) == "e")
+                {
+                    vowelCount++;
+                }
+                if (str.Substring(i, 1) == "i")
+                {
+                    vowelCount++;
+                }
+                if (str.Substring(i, 1) == "o")
+                {
+                    vowelCount++;
+                }
+                if (str.Substring(i, 1) == "u")
+                {
+                    vowelCount++;
+                }
+            }
+
+            return vowelCount;
+        }
+    }
+}
---
+++ More_Code_ExercisesTests1/VowelCountTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using More_Code_Exercises;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace More_Code_Exercises.Tests
+{
+    [TestClass()]
+    public class VowelCountTests
+    {
+        [TestMethod()]
+        public void GetVowelCountTest()
+        {
+
+            Assert.AreEqual(5, VowelCount.GetVowelCount("abracadabra"));
+
+        }
+    }
+}
